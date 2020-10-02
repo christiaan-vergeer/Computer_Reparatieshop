b3e7c51f6802e3eaf1b1a9747e3bf53abd3cf035
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-10-02 15:47:02 +0200

    collour for late enddate

--- DAL/ReparatieInitializer.cs
+++ DAL/ReparatieInitializer.cs
@@ -13,7 +13,13 @@ namespace Computer_Reparatieshop.DAL
         {
             var reparaties = new List<Reparatieopdrachten>
             {
-                new Reparatieopdrachten{Name="test", Startdate = DateTime.Now, Enddate = DateTime.Now, Status = Status.Done}
+                new Reparatieopdrachten{Name="test1", Startdate = DateTime.Parse("2020-10-02"), Enddate = DateTime.Parse("2020-10-22"), Status = Status.Pending},
+                new Reparatieopdrachten{Name="test2", Startdate = DateTime.Parse("2020-10-01"), Enddate = DateTime.Parse("2020-10-22"), Status = Status.Pending},
+                new Reparatieopdrachten{Name="test3", Startdate = DateTime.Parse("2020-10-12"), Enddate = DateTime.Parse("2020-10-22"), Status = Status.Pending},
+                new Reparatieopdrachten{Name="test4", Startdate = DateTime.Parse("2020-10-21"), Enddate = DateTime.Parse("2020-10-22"), Status = Status.InProgress},
+                new Reparatieopdrachten{Name="test5", Startdate = DateTime.Parse("2020-10-12"), Enddate = DateTime.Parse("2020-10-22"), Status = Status.WaitingForParts},
+                new Reparatieopdrachten{Name="test6", Startdate = DateTime.Parse("2020-10-12"), Enddate = DateTime.Parse("2020-10-22"), Status = Status.WaitingForParts},
+                new Reparatieopdrachten{Name="test7", Startdate = DateTime.Parse("2020-11-02"), Enddate = DateTime.Parse("2020-10-22"), Status = Status.Done}
             };
 
             reparaties.ForEach(s => context.reparatieopdrachtens.Add(s));
