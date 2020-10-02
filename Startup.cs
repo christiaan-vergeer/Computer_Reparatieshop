994324f7618d76595947c68ff0542b8d7d536f6a
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-10-02 10:09:37 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Computer_Reparatieshop.Startup))]
+namespace Computer_Reparatieshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
